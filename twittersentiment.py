0733b6fa0d3a5a893387b2a9415ba4b208177ee6
Keoki Seu <kaseu@lbl.gov>
2013-01-31 18:16:44 -0800

    more features

--- twittersentiment.py
+++ twittersentiment.py
@@ -1,11 +1,15 @@
 import twitter
 from senti_classifier import senti_classifier as sc
 import pickle
+import nltk
+from nltk.corpus import stopwords
 
 cl = "/Volumes/Haoma/keoki/datascience/insight-product/repos/sentiment_classifier/src/senti_classifier/classifier-NaiveBayes.tweets.pickle"
 cl = "/scratch/classifier-NaiveBayes.tweets.pickle"
 with open(cl, 'r') as f:
     classifier = pickle.load(f)
+
+stopwords = stopwords.words("english")
 
 def authenticate():
     OAUTH_TOKEN="167133966-EnnTYkbphBbmwo9FFd2hwv5JSkOaNSRSBsh4LzY"
@@ -14,7 +18,7 @@ def authenticate():
     CONSUMER_SECRET = "I0EVikHnnGU6wAzL7gl1nNuejIu2YuUiheleG7DtQIY"
     return twitter.OAuth(OAUTH_TOKEN, OAUTH_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
-def search_tweets(term, auth=None, result_type="recent", limit=9):
+def search_tweets(term, auth=None, result_type="recent", limit=100, lang='en'):
     """ Uses the Twitter API to search for tweets.  Returns a json dict of
     tweets according to the search term.
     """
@@ -27,35 +31,28 @@ def search_tweets(term, auth=None, result_type="recent", limit=9):
     ts = twitter.Twitter(domain="search.twitter.com", auth=auth)
     return ts.search(q=term,
                     result_type="recent",
-                    limit=limit,
+                    rpp=limit,
                     include_entities = 'true')['results']
 
-def filter_tweets(tweets, remove_rt = False, lang='en'):
-    """ filter tweets on criteria. Can either remove if retweet or remove based on lanuage.
-    lang='all' does not do any removal based off lanuage
+def remove_tweets(tweets, remove_rt = True ):
+    """ filter tweets on criteria. Right now it just removes Retweets.
     """
     import re
     if remove_rt:
-        rt_regex = re.compile("RT|[Rr]etweet")
+        rt_regex = re.compile("RT|[Rr]etweet|RETWEET")
     else:
-        rt_regex = None
-
-#    print len(tweets)
+        return tweets
 
     for t in tweets:
-#        print "tweet", t['text'], t['iso_language_code']
         if rt_regex:
             if re.search(rt_regex, t['text']):
-#                print "removing on retweet:", t['text']
+                ol = len(tweets)
                 tweets.remove(t)
+                nl = len(tweets)
+                print 'found RT match', t['text'], ol, nl
                 continue
-        if lang != 'all':
-            if t['iso_language_code'] != lang:
-#                print "removing on language: ", t['iso_language_code']
-                tweets.remove(t)
-                continue
-
-#    print len(tweets)
+            else:
+                print 'no RT match', t['text']
     return tweets
 
 def get_raw_sentiment(tweets):
@@ -73,13 +70,70 @@ def get_raw_sentiment(tweets):
 
     return v
 
-def sort_by_sentiment(tweets, threshold):
+def sort_by_sentiment(tweets):
     """ Sorts tweets by sentiment
     """
+    threshold = 0.5
     st = get_raw_sentiment(tweets)
+    pos = list()
+    neg = list()
+
+    [pos.append(t) if p > threshold else neg.append(t) for (p, n, t) in st]
+
+    return pos, neg
+
+def get_top(tweetlist, filter_term=None, filter_common = True, cutoff=5):
+    """get top words from tweet list.  Filter_term is used to remove terms from list (ie: search terms)
+    """
+    combined_tweets = norm_words(" ".join([t['text'] for t in tweetlist]))
+    if filter_common:
+        combined_tweets = filter(lambda w: not w in stopwords, combined_tweets.split())
+    else:
+        combined_tweets = combined_tweets.split()
+    
+    cutoff_combined = list()
+    for keyword, count in nltk.FreqDist(combined_tweets).iteritems():
+        if count >= cutoff:
+            if keyword != filter_term:
+                cutoff_combined.append((keyword, count))
+    return cutoff_combined
 
 def bag_of_words(words):
     return dict([(word, True) for word in words])
+
+def norm_words(words, lower=True, remove_punctuation = True, remove_http = True):
+    """normalize words by converting to lower case and removing punctuation. Works on both lists of words and a single string, but it's recommenede that this be used on a single string rather than lists of words since norm_words can make some strings empty and it does not remove them.
+    """
+    import string, operator, re
+
+    http_str = re.compile("(http://t.co/)(\w)+ ")    
+    # for now, keep the @ and # since they're special to twitter
+    sp = string.punctuation.replace("#","").replace("@","")
+
+    # convert to string for normalize.
+    if type(words) == unicode:
+        words = words.encode("ascii", errors="ignore")
+
+    if type(words) == str:        
+        if remove_http:
+            words = re.sub(http_str, "", words)
+        if lower:
+            words = words.lower()
+        if remove_punctuation:
+            table = string.maketrans(sp, " "*len(sp))
+            words = words.translate(table)
+    elif type(words) in (list, tuple):
+        if remove_http:
+            words = [re.sub(http_str, "", w) for w in words]
+        if lower:
+            words = [w.lower() for w in words]
+        if remove_punctuation:
+            table = string.maketrans(sp, " "*len(sp))
+            words = [w.translate(table) for w in words]
+    else:
+        print "not list or string, not normalizing"
+
+    return words
 
 def get_sentiment(tweets):
     """
@@ -104,6 +158,15 @@ def search_get_sentiment(term, auth=None):
 
 def search_get_raw_sentiment(term, auth=None):
     tw = search_tweets(term, auth=auth)
-    tw = filter_tweets(tw, remove_rt=True)
+    tw = remove_tweets(tw, remove_rt=True)
     return get_raw_sentiment(tw)
-    
+
+def search_get_sentiment(term, auth=None):
+    raw = search_tweets(term, auth=auth)
+    filtered = remove_tweets(raw)
+    pos, neg = sort_by_sentiment(filtered)
+
+    top_pos = get_top(pos, filter_term=term.lower())
+    top_neg = get_top(neg, filter_term=term.lower())
+    return pos, neg, top_pos, top_neg
+ 
--- wdtt.py
+++ wdtt.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, _app_ctx_stack
 import twittersentiment as ts
+import random
 
 app = Flask(__name__)
 #app.config.from_object = (__name__)
@@ -13,23 +14,45 @@ title = "What does Twitter think?"
 def search_page():
     return render_template("index.html", title=title)
 
-@app.route("/rawsearch", methods=['POST'])
-def rawsearch():
-    # should get (pos_score, neg_score, name, tweet)
-    scores = ts.search_get_raw_sentiment(request.form['search_text'], auth)
-    # scores = (pos, neg, raw_tweet)
-    formatted_scores = []
-    for (p, n, t) in scores:
-        import pprint
-        pprint.pprint(t)
-        formatted_scores.append( (p, n, t['from_user'], t['text']))
-    return render_template('results_raw.html', scores=scores)
+# @app.route("/rawsearch", methods=['POST'])
+# def rawsearch():
+#     scores = ts.search_get_raw_sentiment(request.form['search_text'], auth)
+# #    formatted_scores = [ (p, n, t['from_user'], t['text']) for (p, n, t) in scores ]
+# #    pos, pos_word, neg, neg_words
+#     return render_template('results_raw.html', scores=formatted_scores)
+
 
 @app.route("/search", methods=['POST'])
 def search():
-    # good, bad, irrelevant
-    g, b, i = ts.search_get_sentiment(request.form['search_text'], auth)
-    return render_template('results.html', good=g, bad=b, irr=i)
+    term = request.form['search_text']
+    pos, neg, top_pos, top_neg = ts.search_get_sentiment(term, auth)
+    p = [ (t['from_user'], t['text']) for t in pos]
+    n = [ (t['from_user'], t['text']) for t in neg]
+
+    pos_words = ["amazing", "good", "great", "awesome", "fabulous", "terrific", "rad", "sweet"]
+    neg_words = ["terrible", "poor", "sucks", "smelly", "bad", "awful", "icky"]
+
+    if len(pos) > len(neg):
+        words = pos_words
+        color = "#67FF00"
+    else:
+        words = neg_words
+        color = "#FF0083"
+
+    text = dict()
+    text['term'] = term
+    text['text_result'] = words[random.randint(0, len(words)-1)]
+    text['color'] = color
+
+#    print pos, top_pos
+    stats = dict()
+    stats['pos'] = len(pos)
+    stats['neg'] = len(neg)
+    stats['top_pos'] = len(top_pos)
+    stats['top_neg'] = len(top_neg)
+
+    return render_template("results.html", pos=p, pos_words=top_pos, neg=n, neg_words=top_neg, stats=stats, text=text)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
